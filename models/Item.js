2c4a3145093c1b889099e80f008d5328d60d2dac
Jennifer <Jennifer@MacBook-Pro.local>
2020-08-31 22:24:24 -0500

    fixed correct scrape path

--- models/Item.js
+++ models/Item.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
-    id: {type: String, required: true, unique: true},
-    brand_name: {type: String, required: true},
-    product_title: {type: String, required: true},
-    price: {type: Number, required: true},
-    model_number: {type: String, required: true},
-    link: {type: String, trim: true}
+    // id: {type: String, required: true, unique: true},
+    //  brand_name: {type: String, required: true},
+    product_title: {type: String, required: true, unique: true},
+    //  price: {type: Number, required: true},
+    // model_number: {type: String, required: true},
+    //  link: {type: String, trim: true}
 })
 const Item = mongoose.model("Item", itemSchema);
 
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const routes = require("./routes");
+//const routes = require("./routes");
 const mongoose = require("mongoose");
 const cheerio = require("cheerio");
 const axios = require("axios");
@@ -15,13 +15,12 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-app.use(routes);
+//app.use(routes);
 //database
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/walmart-scraper",{useNewUrlParser: true}
-    
+mongoose.connect(  
+    process.env.MONGODB_URI || "mongodb://localhost/walmart-scraper",{useNewUrlParser: true, useUnifiedTopology: true} 
  )
-
+mongoose.set("useCreateIndex", true);
 //Routes
 // A GET route for scraping the website
 app.get("/scrape", function(req, res) {
@@ -29,10 +28,27 @@ app.get("/scrape", function(req, res) {
   axios.get("http://www.walmart.com/browse/toys/lego/4171_4186_1105635_2927326").then(function(response) {
   // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
-  // Now, we grab every h2 within an article tag
-    $(".prod-brandName product-brand product-brand-desktop-buy-box-update").each(function(i, element) {
+  // 
+    $(".search-result-gridview-item-wrapper a.product-title-link").each(function(i, element) {
       // Save an empty result object
       var result = {};
+      result.product_title = $(this)
+        .text();
+          
+  
+  db.Item.create(result)
+        .then(function(dbItem) {
+          //Show item in console
+          console.log(dbItem);
+        })
+        .catch(function(err) {
+          //Display error
+          console.log(err);
+        });
+      })
+      res.send("Site Scraped");
+})
+})
 app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
   })
